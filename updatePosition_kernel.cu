384411e76b27eeae79bbca432253ea1d63babf6b
kylefritz <kylefritz@a64c3fea-df42-11dd-a37d-c9d1c40e0883>
2009-01-17 21:41:06 +0000

    OpenGL now uses particle positions from the GPU (no memcopy). Particles now exist for [0, border] rather than [-border, border].

--- updatePosition_kernel.cu
+++ updatePosition_kernel.cu
@@ -1,47 +1,53 @@
 
-__global__ void updatePosition(Vector3D boundary, Particle* particleArray)
+__global__ void updatePosition(float4* spheres, Vector3D boundary, Particle* particleArray)
 {
 	// Get id for current particle
     unsigned int id = blockIdx.x*blockDim.x + threadIdx.x;
 
     // Update particle position
-    particleArray[id].position.x = particleArray[id].position.x + particleArray[id].velocity.x;
-    particleArray[id].position.y = particleArray[id].position.y + particleArray[id].velocity.y;
-    particleArray[id].position.z = particleArray[id].position.z + particleArray[id].velocity.z;
+    float x = particleArray[id].position.x + particleArray[id].velocity.x;
+    float y = particleArray[id].position.y + particleArray[id].velocity.y;
+    float z = particleArray[id].position.z + particleArray[id].velocity.z;
 
     // Boundary check
-    if(particleArray[id].position.x < -boundary.x)
+    if(x < 0)
     {
-    	particleArray[id].position.x = - boundary.x - particleArray[id].position.x- boundary.x;
-    	particleArray[id].velocity.x = - particleArray[id].velocity.x;
+    	x = -x;
+    	particleArray[id].velocity.x = -particleArray[id].velocity.x;
     }
-    else if(particleArray[id].position.x > boundary.x)
+    else if(x > boundary.x)
     {
-    	particleArray[id].position.x = boundary.x - (particleArray[id].position.x - boundary.x);
-    	particleArray[id].velocity.x = - particleArray[id].velocity.x;
-    }
-
-    if(particleArray[id].position.y < -boundary.y)
-    {
-    	particleArray[id].position.y = - boundary.y - particleArray[id].position.y - boundary.y;
-    	particleArray[id].velocity.y = - particleArray[id].velocity.y;
-    }
-    else if(particleArray[id].position.y > boundary.y)
-    {
-    	particleArray[id].position.y = boundary.y - (particleArray[id].position.y - boundary.y);
-    	particleArray[id].velocity.y = - particleArray[id].velocity.y;
+    	x = boundary.x - (x - boundary.x);
+    	particleArray[id].velocity.x = -particleArray[id].velocity.x;
     }
 
-    if(particleArray[id].position.z < -boundary.z)
+    if(y < 0)
     {
-    	particleArray[id].position.z = - boundary.z - particleArray[id].position.z - boundary.z;
-    	particleArray[id].velocity.z = - particleArray[id].velocity.z;
+    	y = -y;
+    	particleArray[id].velocity.y = -particleArray[id].velocity.y;
     }
-    else if(particleArray[id].position.z > boundary.z)
+    else if(y > boundary.y)
     {
-    	particleArray[id].position.z = boundary.z - (particleArray[id].position.z - boundary.z);
-    	particleArray[id].velocity.z = - particleArray[id].velocity.z;
+    	y = boundary.y - (y - boundary.y);
+    	particleArray[id].velocity.y = -particleArray[id].velocity.y;
     }
+
+    if(z < 0)
+    {
+    	z = -z;
+    	particleArray[id].velocity.z = -particleArray[id].velocity.z;
+    }
+    else if(z > boundary.z)
+    {
+    	z = boundary.z - (z - boundary.z);
+    	particleArray[id].velocity.z = -particleArray[id].velocity.z;
+    }
+
+    makeSphere(spheres, id, x - 16, y - 16, z - 16, 0.5f);
+
+    particleArray[id].position.x = x;
+    particleArray[id].position.y = y;
+    particleArray[id].position.z = z;
 
 	__syncthreads();
 }
